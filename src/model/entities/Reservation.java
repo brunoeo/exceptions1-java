4c3ed2a4fb1affc6353701f92de636833991a447
bruno eLIAS <bruno-150@hotmail.com>
2021-03-17 10:08:27 -0300

    Good soliction

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainExcepition;
+
 public class Reservation {
 	
 	private Integer roomNuber;
@@ -11,12 +13,11 @@ public class Reservation {
 	private Date checkOut;
 	
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-	
-	public Reservation() {
-		
-	}
-	
+
 	public Reservation(Integer roomNuber, Date checkIn, Date checkOut) {
+		if (!checkOut.after(checkIn)) {
+			throw new DomainExcepition("Check-out date must be after check-in date");
+			}
 		this.roomNuber = roomNuber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -44,19 +45,18 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 	
-	public String updateDates(Date checkIn, Date checkOut) {
+	public void updateDates(Date checkIn, Date checkOut) {
 		Date now = new Date();
 		if(checkIn.before(now) || checkOut.before(now)) {
-			return "Reservation dates for update must be future dates";
+			throw new DomainExcepition("Reservation dates for update must be future dates");
 		}
-			if (!checkOut.after(checkIn)) {
-				return "Check-out date must be after check-in date";
-				}
+		if (!checkOut.after(checkIn)) {
+			throw new DomainExcepition("Check-out date must be after check-in date");
+			}
 			
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-		return null;
-	}
+		}
 	
 	@Override
 	public String toString() {
---
+++ src/model/exceptions/DomainExcepition.java
@@ -0,0 +1,9 @@
+package model.exceptions;
+
+public class DomainExcepition extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public DomainExcepition(String msg) {
+		super(msg);
+	}
+}
